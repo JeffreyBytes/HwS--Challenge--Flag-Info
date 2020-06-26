9aec75233d67f4dd39fdecc48db61a4e5dd8926f
Jeffrey C <spaz926@me.com>
2020-06-26 18:24:47 -0500

    Initial Commit

---
+++ Challenge-FlagInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challenge-FlagInfo
+//
+//  Created by Jeffrey Carpenter on 6/26/20.
+//  Copyright © 2020 Jeffrey Carpenter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
